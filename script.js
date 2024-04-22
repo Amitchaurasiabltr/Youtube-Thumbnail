398de4085bde539f83521ce19ae15ec9c6f8f681
Amit Chaurasia <amitchaurasiabltr@gmail.com>
2024-04-23 04:49:31 +0530

    Commit
    Date: 23-04-2024

---
+++ script.js
@@ -0,0 +1,27 @@
+function createCard(titel, cName, views, monthsOld, duration, thumbnail) {
+    //Finish this function
+    let viewStr
+    if (views < 1000000) {
+        viewStr = views / 1000 + "k"
+    }
+    else if (views > 1000000) {
+        viewStr = views / 1000000 + "M"
+    }
+    else (
+        viewStr = views / 1000 + "k"
+    )
+    let html = `<div class="card">
+    <div class="image">
+        <img src="${thumbnail}"
+            alt="">
+        <div class="capsule">${duration}</div>
+    </div>
+    <div class="text">
+        <h1>${titel}</h1>
+        <p>${cName} . ${viewStr} views . ${monthsOld} month ago</p>
+    </div>
+</div>`
+    document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
+}
+
+createCard("Introduction to my video | Welcome to new One #2", "National Academy", 780000, 7, "31:22", "https://ergonotes.com/wp-content/uploads/2022/11/Find-YouTube-Thumbnail-Source.jpg")
